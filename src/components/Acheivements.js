4bcff69b0bd23aa5a72c30c27531ceb8da69be6b
anshu <manu22425@gmail.com>
2023-12-15 03:37:09 +0530

    Acheivements Page done

--- src/components/Acheivements.js
+++ src/components/Acheivements.js
@@ -3,16 +3,20 @@ import acheivements from '../utils/acheivements'
 
 const Acheivements = () => {
     return (
-        <div className='container mx-auto p-5'>
+        <>
             <p className='text-2xl text-center font-bold text-blue-500'>Acheivements</p>
-            {acheivements && acheivements.map((acheivement) => (
-                <h1>
-                    {acheivement.name}
-                </h1>
+            <div className='container mx-auto p-5'>
+                {acheivements && acheivements.map((acheivement) => (
+                   <a href={acheivement.link} target="_blank">
+                     <div className="grid grid-cols-1 m-2 border-2 border-gray-300 rounded-md border-solid  p-3 hover:bg-black hover:text-white hover:border-yellow-700 hover:border-3">
+                        <p className='text-xl font-bold'>{acheivement.title}</p>
+                        <p className='text-blue-500 text-lg font-semibold'>{acheivement.name}</p>
+                    </div>
+                   </a>
+                ))}
+            </div>
+        </>
 
-            ))}
-           <iframe src="https://www.hackerrank.com/certificates/iframe/de2674c57f1e"  title="Iframe Example"></iframe>
-        </div>
     )
 }
 
--- src/utils/acheivements.js
+++ src/utils/acheivements.js
@@ -1,11 +1,63 @@
 const acheivements = [
     {
         id:1,
-        name: "dfa"
+        title: "Reached Pro Level in Competitve Coding with 7410Xp and 2705 reputation",
+        name: "Edabit",
+        link: "https://edabit.com/user/6QHXQkQAcXMJ7C2kK"
     },
     {
         id:2,
-        name: "vxzvs"
+        title: "4 star Coder in Python",
+        name: "HackerRank",
+        link: "https://www.hackerrank.com/profile/anshu_patel_001"
+    },
+     {
+        id:3,
+        title: "3 star in Problem Solving",
+        name: "HackerRank",
+        link: "https://www.hackerrank.com/profile/anshu_patel_001"
+    },
+    {
+        id:4,
+        title: "Solved 100+ problems on LeetCode",
+        name: "LeetCode",
+        link: "https://leetcode.com/manu22425/"
+    },
+    {
+        id:5,
+        title: "Certified in Problem Solving",
+        name: "HackerRank",
+        link: "https://www.hackerrank.com/certificates/de2674c57f1e"
+    },
+    {
+        id:5,
+        title: "Certified in Python",
+        name: "HackerRank",
+        link: "https://www.hackerrank.com/certificates/a6175eb7c793"
+    },
+    {
+        id:5,
+        title: "Certified in React",
+        name: "HackerRank",
+        link: "https://www.hackerrank.com/certificates/eebcc0808f53"
+    },
+    {
+        id:5,
+        title: "Certified in JavaScript",
+        name: "HackerRank",
+        link: "https://www.hackerrank.com/certificates/b375167c80c0"
+    },
+    {
+        id:6,
+        title: "Skill badge in PHP",
+        name: "HackerRank",
+        link: "https://www.linkedin.com/in/anshu-patel-2069291a7/"
+    },
+    {
+        id:5,
+        title: "Skill badge in Python",
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/anshu-patel-2069291a7/"
     }
 ]
 
